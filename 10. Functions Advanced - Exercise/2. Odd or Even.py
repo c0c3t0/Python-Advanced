3f518e9c8d82964795447ae5687a0d004d91cc08
c0c3t0 <98971277+c0c3t0@users.noreply.github.com>
2022-09-19 14:53:49 +0300

    Functions Advanced

--- 10. Functions Advanced - Exercise/2. Odd or Even.py
+++ 10. Functions Advanced - Exercise/2. Odd or Even.py
@@ -1,11 +1,7 @@
 command = input()
 numbers = [int(el) for el in input().split()]
 
-
 odd = sum(filter(lambda a: a % 2 != 0, numbers))
 even = sum(filter(lambda a: a % 2 == 0, numbers))
 
-if command == "Odd":
-    print(odd * len(numbers))
-else:
-    print(even * len(numbers))
+print(odd * len(numbers)) if command == "Odd" else print(even * len(numbers))
