fcc8fddafcf884594430ae5ccef8e33afbf9ef47
c0c3t0 <98971277+c0c3t0@users.noreply.github.com>
2022-09-19 14:53:09 +0300

    Functions Advanced

--- 10. Functions Advanced - Exercise/1. Negative vs Positive.py
+++ 10. Functions Advanced - Exercise/1. Negative vs Positive.py
@@ -6,7 +6,5 @@ negatives = sum(filter(lambda a: a < 0, seq))
 print(negatives)
 print(positives)
 
-if abs(negatives) > positives:
-    print("The negatives are stronger than the positives")
-else:
-    print("The positives are stronger than the negatives")
+print("The negatives are stronger than the positives") if abs(negatives) > positives else print(
+    "The positives are stronger than the negatives")
